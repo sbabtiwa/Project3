e76a7fb3c2408fa30e254c7b17d484e6202d32f0
Sanika Babtiwale <sbabtiwa@umich.edu>
2017-03-19 17:19:51 -0400

    Adding final version of file

--- SI206W17_HW6.py
+++ SI206W17_HW6.py
@@ -161,7 +161,7 @@ print("\n\n***** Problem 9 *****")
 
 ## Note that you can use another list you have already created for this problem.
 
-names_with_not_too_much_seniority = [x.name for x in programmers if len(x.name) >= x.years_UM]
+names_with_not_too_much_seniority = [x.name for x in programmers if len(x.name) > x.years_UM]
 print(names_with_not_too_much_seniority)
 
 
